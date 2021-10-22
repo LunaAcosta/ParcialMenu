44045181eccb7086e75c7c3e701abcd1ca463f22
Luna Acosta <lunaacosta300@gmail.com>
2021-10-21 22:52:02 -0600

    Agregar archivos de proyecto.

---
+++ ParcialMenu/FmrAcceso.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace ParcialMenu
+{
+    partial class FmrAcceso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.Label();
+            this.mtbNombreUsuario = new MetroFramework.Controls.MetroTextBox();
+            this.mtbContraseña = new MetroFramework.Controls.MetroTextBox();
+            this.btnAcceder = new MetroFramework.Controls.MetroButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mrlCrerUsuario = new MetroFramework.Controls.MetroLink();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(188, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INICIO DE SESION";
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Contraseña.AutoSize = true;
+            this.Contraseña.BackColor = System.Drawing.Color.Transparent;
+            this.Contraseña.ForeColor = System.Drawing.Color.White;
+            this.Contraseña.Location = new System.Drawing.Point(81, 246);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.Contraseña.TabIndex = 1;
+            this.Contraseña.Text = "Contraseña";
+            // 
+            // mtbNombreUsuario
+            // 
+            this.mtbNombreUsuario.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            // 
+            // 
+            // 
+            this.mtbNombreUsuario.CustomButton.Image = null;
+            this.mtbNombreUsuario.CustomButton.Location = new System.Drawing.Point(283, 1);
+            this.mtbNombreUsuario.CustomButton.Name = "";
+            this.mtbNombreUsuario.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtbNombreUsuario.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtbNombreUsuario.CustomButton.TabIndex = 1;
+            this.mtbNombreUsuario.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtbNombreUsuario.CustomButton.UseSelectable = true;
+            this.mtbNombreUsuario.CustomButton.Visible = false;
+            this.mtbNombreUsuario.Lines = new string[0];
+            this.mtbNombreUsuario.Location = new System.Drawing.Point(84, 131);
+            this.mtbNombreUsuario.MaxLength = 32767;
+            this.mtbNombreUsuario.Name = "mtbNombreUsuario";
+            this.mtbNombreUsuario.PasswordChar = '\0';
+            this.mtbNombreUsuario.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtbNombreUsuario.SelectedText = "";
+            this.mtbNombreUsuario.SelectionLength = 0;
+            this.mtbNombreUsuario.SelectionStart = 0;
+            this.mtbNombreUsuario.ShortcutsEnabled = true;
+            this.mtbNombreUsuario.Size = new System.Drawing.Size(305, 23);
+            this.mtbNombreUsuario.TabIndex = 3;
+            this.mtbNombreUsuario.UseSelectable = true;
+            this.mtbNombreUsuario.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtbNombreUsuario.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // mtbContraseña
+            // 
+            // 
+            // 
+            // 
+            this.mtbContraseña.CustomButton.Image = null;
+            this.mtbContraseña.CustomButton.Location = new System.Drawing.Point(283, 1);
+            this.mtbContraseña.CustomButton.Name = "";
+            this.mtbContraseña.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.mtbContraseña.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.mtbContraseña.CustomButton.TabIndex = 1;
+            this.mtbContraseña.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.mtbContraseña.CustomButton.UseSelectable = true;
+            this.mtbContraseña.CustomButton.Visible = false;
+            this.mtbContraseña.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.mtbContraseña.Lines = new string[0];
+            this.mtbContraseña.Location = new System.Drawing.Point(84, 278);
+            this.mtbContraseña.MaxLength = 32767;
+            this.mtbContraseña.Name = "mtbContraseña";
+            this.mtbContraseña.PasswordChar = '\0';
+            this.mtbContraseña.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.mtbContraseña.SelectedText = "";
+            this.mtbContraseña.SelectionLength = 0;
+            this.mtbContraseña.SelectionStart = 0;
+            this.mtbContraseña.ShortcutsEnabled = true;
+            this.mtbContraseña.Size = new System.Drawing.Size(305, 23);
+            this.mtbContraseña.TabIndex = 4;
+            this.mtbContraseña.UseSelectable = true;
+            this.mtbContraseña.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.mtbContraseña.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnAcceder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAcceder.Location = new System.Drawing.Point(140, 350);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(189, 23);
+            this.btnAcceder.TabIndex = 5;
+            this.btnAcceder.Text = "Continuar";
+            this.btnAcceder.UseCustomBackColor = true;
+            this.btnAcceder.UseCustomForeColor = true;
+            this.btnAcceder.UseSelectable = true;
+            this.btnAcceder.UseStyleColors = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(113, 443);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Crear un Usuario ?";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(81, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombre Del Usuario";
+            // 
+            // mrlCrerUsuario
+            // 
+            this.mrlCrerUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.mrlCrerUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.mrlCrerUsuario.Location = new System.Drawing.Point(214, 432);
+            this.mrlCrerUsuario.Name = "mrlCrerUsuario";
+            this.mrlCrerUsuario.Size = new System.Drawing.Size(105, 36);
+            this.mrlCrerUsuario.TabIndex = 8;
+            this.mrlCrerUsuario.Text = "Crear Un Usuario";
+            this.mrlCrerUsuario.UseCustomBackColor = true;
+            this.mrlCrerUsuario.UseCustomForeColor = true;
+            this.mrlCrerUsuario.UseSelectable = true;
+            this.mrlCrerUsuario.UseStyleColors = true;
+            // 
+            // FmrAcceso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::ParcialMenu.Properties.Resources.abstract_high_tech_security_background_vector1;
+            this.ClientSize = new System.Drawing.Size(446, 503);
+            this.Controls.Add(this.mrlCrerUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.mtbContraseña);
+            this.Controls.Add(this.mtbNombreUsuario);
+            this.Controls.Add(this.Contraseña);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FmrAcceso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Contraseña;
+        private MetroFramework.Controls.MetroTextBox mtbNombreUsuario;
+        private MetroFramework.Controls.MetroTextBox mtbContraseña;
+        private MetroFramework.Controls.MetroButton btnAcceder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private MetroFramework.Controls.MetroLink mrlCrerUsuario;
+    }
+}
---
+++ ParcialMenu/Menu.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace ParcialMenu
+{
+    partial class FmrMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmrMenu));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRegistro = new MetroFramework.Controls.MetroButton();
+            this.btnUsurarios = new MetroFramework.Controls.MetroButton();
+            this.btnPerfil = new MetroFramework.Controls.MetroButton();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido !";
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRegistro.BackgroundImage")));
+            this.btnRegistro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRegistro.Location = new System.Drawing.Point(212, 299);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(235, 91);
+            this.btnRegistro.TabIndex = 3;
+            this.btnRegistro.Text = "REGISTROS";
+            this.btnRegistro.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnRegistro.UseCustomBackColor = true;
+            this.btnRegistro.UseCustomForeColor = true;
+            this.btnRegistro.UseSelectable = true;
+            this.btnRegistro.UseStyleColors = true;
+            // 
+            // btnUsurarios
+            // 
+            this.btnUsurarios.BackColor = System.Drawing.Color.White;
+            this.btnUsurarios.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUsurarios.BackgroundImage")));
+            this.btnUsurarios.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnUsurarios.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnUsurarios.Location = new System.Drawing.Point(423, 153);
+            this.btnUsurarios.Name = "btnUsurarios";
+            this.btnUsurarios.Size = new System.Drawing.Size(176, 116);
+            this.btnUsurarios.TabIndex = 4;
+            this.btnUsurarios.Text = "USUARIOS";
+            this.btnUsurarios.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.btnUsurarios.UseCustomBackColor = true;
+            this.btnUsurarios.UseCustomForeColor = true;
+            this.btnUsurarios.UseSelectable = true;
+            this.btnUsurarios.UseStyleColors = true;
+            this.btnUsurarios.UseWaitCursor = true;
+            // 
+            // btnPerfil
+            // 
+            this.btnPerfil.AllowDrop = true;
+            this.btnPerfil.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPerfil.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPerfil.BackgroundImage")));
+            this.btnPerfil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPerfil.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnPerfil.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnPerfil.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPerfil.Location = new System.Drawing.Point(86, 153);
+            this.btnPerfil.Name = "btnPerfil";
+            this.btnPerfil.Size = new System.Drawing.Size(146, 116);
+            this.btnPerfil.TabIndex = 2;
+            this.btnPerfil.Text = "PERFIL";
+            this.btnPerfil.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnPerfil.UseCustomBackColor = true;
+            this.btnPerfil.UseCustomForeColor = true;
+            this.btnPerfil.UseSelectable = true;
+            this.btnPerfil.UseStyleColors = true;
+            this.btnPerfil.UseWaitCursor = true;
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(691, 25);
+            this.toolStrip.TabIndex = 5;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // FmrMenu
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(691, 450);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.btnUsurarios);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.btnPerfil);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FmrMenu";
+            this.RightToLeftLayout = true;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton btnPerfil;
+        private MetroFramework.Controls.MetroButton btnRegistro;
+        private MetroFramework.Controls.MetroButton btnUsurarios;
+        private System.Windows.Forms.ToolStrip toolStrip;
+    }
+}
